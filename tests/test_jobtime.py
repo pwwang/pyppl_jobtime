7868abf7d9c8fb6c26682e3994b089302a0817e9
pwwang <pwwang@pwwang.com>
2019-12-27 18:50:21 -0600

    Update tests

--- tests/test_jobtime.py
+++ tests/test_jobtime.py
@@ -14,7 +14,7 @@ def test_jobtime(tmp_path):
 		input = {'a':[.5,.8,1.2,1.7,2]},
 		output = 'a:1', script = 'sleep {{i.a}}')
 	PyPPL().start(pJobTime).run()
-	sys.argv[1:] = ['jobtime', '--wdir', str(tmp_path), '--proc', 'pJobTime', '--outfile', str(tmp_path/'jobtime.png')]
+	sys.argv = ['pyppl', 'jobtime', '--wdir', str(tmp_path), '--proc', 'pJobTime', '--outfile', str(tmp_path/'jobtime.png')]
 	console.main()
 
 @pytest.mark.parametrize('string,rdata, ignoreintkey', [
